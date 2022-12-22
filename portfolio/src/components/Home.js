29dce1c11c2b85680d2bd730e398e8b0535fb044
RozkeAdine <rozkeadine@gmail.com>
2022-12-22 14:19:22 +0100

    add link

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -3,7 +3,11 @@ import '../styles/Home.css';
 function Home () {
     return (
         <div className='pra-home'>
-            <span>site réalisé avec REACT</span><br/>
+            <span>
+                <button>
+                <a href="http://portfoliorozkeadine.com/" target="_blank">Version HTML</a>
+                </button>
+            </span><br/>
             <h1>
                 En recherche de stage de 3 mois minimum<br/>
                 <div className="pra-name">Rozke Adine BOUYAHIA</div>
